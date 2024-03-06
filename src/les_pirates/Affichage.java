1e453f44a1d742e7de6f75100e7bd614965a3174
lescaich <lescaich@btslivh.eu>
2024-03-06 09:58:49 +0100

    Ajout classe Affichage

---
+++ src/les_pirates/Affichage.java
@@ -0,0 +1,14 @@
+package les_pirates;
+
+public class Affichage {
+
+	public Affichage() {
+		// The display class has no attributes
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
