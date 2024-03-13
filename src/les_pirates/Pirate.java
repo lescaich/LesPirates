8e1a2fa9cdb95b98f5180f162d4c9585b91aa43b
lescaich <lescaich@btslivh.eu>
2024-03-13 08:52:40 +0100

    Changement attribut position Pirate

--- src/les_pirates/Pirate.java
+++ src/les_pirates/Pirate.java
@@ -2,10 +2,10 @@ package les_pirates;
 
 public class Pirate {
 	
-	private int PV;
+	private int PV = 5;
 	private String nom;
 	private Couleur couleur;
-	private Case caseActuelle;
+	private int position;
 	
 	
 	public Pirate() {
@@ -23,9 +23,9 @@ public class Pirate {
 	public Couleur getCouleur() {
 		return couleur;
 	}
-	
-	public Case getCaseActuelle() {
-		return caseActuelle;
+
+	public int getPosition() {
+		return position;
 	}
 	
 	
