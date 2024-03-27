b8c54a7e53fe6ee3932a63c5f2c9c7f71d63e5e3
lescaich <lescaich@btslivh.eu>
2024-03-27 18:29:47 +0100

    Ajout classe De

---
+++ src/les_pirates/De.java
@@ -0,0 +1,15 @@
+package les_pirates;
+
+import java.util.Random;
+
+public class De {
+
+	public De() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected int lanceDe(Random random) {
+		return 0;
+	}
+	
+}
