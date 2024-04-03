6c8f590e7fa5f652dadf3d4c0a7525b040dfbe19
lescaich <lescaich@btslivh.eu>
2024-04-04 00:01:57 +0200

    Dernier commit

--- src/les_pirates/Affichage.java
+++ src/les_pirates/Affichage.java
@@ -11,13 +11,18 @@ public class Affichage {
 		Case case1 = new Case(1);
 		Case case2 = new Case(2);
 		Pirate pirate = new Pirate(5, "Bill", Couleur.BLEU);
-		System.out.println(case0.getNumero());
-		System.out.println(case1.getNumero());
-		System.out.println();
+		//System.out.println(case0.getNumero());
+		//System.out.println(case1.getNumero());
+		//System.out.println();
+		System.out.println("Position Bill : ");
 		System.out.println(pirate.getPosition());
+		System.out.println("Bill avance d'1 case. ");
 		pirate.SeDeplace(1);
+		System.out.println("Position Bill : ");
 		System.out.println(pirate.getPosition());
+		System.out.println("Bill avance d'1 case. ");
 		pirate.SeDeplace(1);
+		System.out.println("Position Bill : ");
 		System.out.println(pirate.getPosition());
 	}
 	
